e32901b13d995b6c137c5c30c13b0af067fbd811
xudb <15555537368@163.com>
2021-07-13 16:03:26 +0800

    -

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,53 @@
-module.exports = {
+const { defineConfig } = require('eslint-define-config')
+
+module.exports = defineConfig({
     root: true,
+    extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'react-hooks'],
-    extends: [
-        // "standard",
-        // "plugin:@typescript-eslint/eslint-recommended",
-        // "plugin:@typescript-eslint/recommended",
-        'standard-with-typescript',
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:node/recommended',
-        'plugin:@typescript-eslint/recommended',
-        // "prettier", // 暂时不开启prettier的eslint规则
-        // "prettier/@typescript-eslint",
-        // "prettier/react"
-    ],
     parserOptions: {
         sourceType: 'module',
         ecmaVersion: 2020,
     },
-    env: {
-        es6: true,
-        node: true,
-    },
     rules: {
-        '@typescript-eslint/no-var-requires': 0,
-        '@typescript-eslint/promise-function-async': 0,
-        'no-callback-literal': 0,
-        '@typescript-eslint/indent': ['error', 4],
-        '@typescript-eslint/quotes': ['error', 'single'],
-        '@typescript-eslint/camelcase': 0,
-        '@typescript-eslint/require-await': 'warn',
-        '@typescript-eslint/no-floating-promises': ['warn'],
-        '@typescript-eslint/no-empty-function': ['warn'],
-        '@typescript-eslint/consistent-type-assertions': 0,
-        '@typescript-eslint/require-await': 0,
-        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-        'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-        'space-before-function-paren': [
-            'error',
+        'no-debugger': ['error'],
+        'no-empty': ['warn', { allowEmptyCatch: true }],
+        'no-process-exit': 'off',
+        'no-useless-escape': 'off',
+        'prefer-const': [
+            'warn',
             {
-                anonymous: 'never',
-                named: 'never',
-                asyncArrow: 'always',
+                destructuring: 'all',
             },
         ],
-        'generator-star-spacing': ['error', { before: false, after: true }],
+
+        'node/no-missing-import': [
+            'error',
+            {
+                allowModules: ['types', 'estree', 'testUtils', 'stylus'],
+                tryExtensions: ['.ts', '.js', '.jsx', '.tsx', '.d.ts'],
+            },
+        ],
+        'node/no-missing-require': [
+            'error',
+            {
+                // for try-catching yarn pnp
+                allowModules: ['pnpapi'],
+                tryExtensions: ['.ts', '.js', '.jsx', '.tsx', '.d.ts'],
+            },
+        ],
+        'node/no-deprecated-api': 'off',
+        'node/no-unpublished-import': 'off',
+        'node/no-unpublished-require': 'off',
+        'node/no-unsupported-features/es-syntax': 'off',
+
+        '@typescript-eslint/ban-ts-comment': 'off', // TODO: we should turn this on in a new PR
+        '@typescript-eslint/ban-types': 'off', // TODO: we should turn this on in a new PR
+        '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
+        '@typescript-eslint/no-explicit-any': 'off', // maybe we should turn this on in a new PR
+        '@typescript-eslint/no-extra-semi': 'off', // conflicts with prettier
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off', // maybe we should turn this on in a new PR
+        '@typescript-eslint/no-unused-vars': 'off', // maybe we should turn this on in a new PR
+        '@typescript-eslint/no-var-requires': 'off',
     },
-}
+})
