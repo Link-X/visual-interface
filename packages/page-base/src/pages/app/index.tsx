c41e3671f28afb0cba1787b2eefb8a32d840b5e5
xudb <15555537368@163.com>
2021-07-14 15:28:27 +0800

    -

--- packages/page-base/src/pages/app/index.tsx
+++ packages/page-base/src/pages/app/index.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 import { renderRoutes } from 'react-router-config'
 
 const Index = (props) => {
-  return <div className="big-data_box">{renderRoutes(props.route.routes)}</div>
+    console.log(props)
+    return <div className="big-data_box">{renderRoutes(props.route.routes)}</div>
 }
 
 export default Index
-
