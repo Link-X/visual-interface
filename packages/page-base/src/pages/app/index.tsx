f331627080bb02f18a380f505d895ca8637c4b03
xudb <15555537368@163.com>
2021-07-14 15:37:19 +0800

    -

--- packages/page-base/src/pages/app/index.tsx
+++ packages/page-base/src/pages/app/index.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { renderRoutes } from 'react-router-config'
 
 const Index = (props) => {
-    console.log(props, e)
     return <div className="big-data_box">{renderRoutes(props.route.routes)}</div>
 }
 
